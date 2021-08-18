64617d328d02a01d949b0f5b147a0eb7dee9f77d
Sakshi <sakshishetty1231997@gmail.com>
2021-08-18 20:56:13 +0530

    Added: Welcome Message to the program

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To create a Linked List
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-18
+ */
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Linked List Program");
+    }
+}
